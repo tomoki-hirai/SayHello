59d84cdd49623bc037b950adc4f392823cffd23b
tomoki-hirai <thirai@mikilab.doshisha.c.jp>
2018-06-15 16:34:56 +0900

    ファイルのPOST機能を実装

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// post
+router.post('/',function(req,res){
+  // console.log(req);
+  // res.send('ok')
+
+  let buffers = [];
+    let cnt = 0;
+
+    req.on('data', (chunk) => {
+        buffers.push(chunk);
+        console.log(++cnt);
+    });
+
+    req.on('end', () => {
+        console.log(`[done] Image upload`);
+        req.rawBody = Buffer.concat(buffers);
+        //書き込み
+        fs.writeFile('./data/img.mp3', req.rawBody, 'utf-8',(err) => {
+            if(err) return;
+            console.log(`[done] Image save`);
+        });
+    });
+    res.send('')
+})
 
 module.exports = router;
